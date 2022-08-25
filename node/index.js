3f09d05588b1a4b02734546e56024fa6fe57de7f
Lucas Coronel <lccoronel7@icloud.com>
2022-08-25 10:22:05 -0300

    ➕ setup mysql database with node

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mysql = require('mysql')
 
+const config = {
+   host: 'db',
+   user: 'root',
+   password: 'root',
+   database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config)
 const app = express()
+
+const sql = `INSERT INTO people(name) values('Lucas')`
+connection.query(sql)
+connection.end() 
 
 app.get('/', (req, res) => {
    res.send('<h1>Lucas Coronel<h1>')
