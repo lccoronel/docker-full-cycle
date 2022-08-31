1e97169f7004386470b2a9ba07fc6d6318c1fc55
Lucas Coronel <lccoronel7@icloud.com>
2022-08-31 08:24:54 -0300

    ✨ list users page

--- node-challenge/index.js
+++ node-challenge/index.js
@@ -1,14 +1,40 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const app = express()
+const config = {
+   host: 'database',
+   user: 'root',
+   password: 'root',
+   database: 'node_challenge'
+}
+
+const database = mysql.createConnection(config)
+
+const insertUserSQL = `INSERT INTO user(name) values('Lino')`
+database.query(insertUserSQL)
 
 app.get('/', (req, res) => {
-   res.send(`
-      <h1>Full Cycle Rocks!</h1> 
-      </p>
-         <p> - Lista de nomes cadastrada no banco de dados.</p>
-      <p>
-   `)
+   database.query(`SELECT * from user`, (err, results) => {
+      if (err) {
+         return res.send('<h1>Opa, tivemos um erro ao listar os nomes dos usuarios</h1>')
+      }
+   
+      return res.send(`
+         <h1>Full Cycle Rocks!</h1> 
+
+         <p> - Lista de nomes cadastrados no banco de dados.</p>
+
+         <ul>
+            ${results.map(user => (
+               `<li>${user.name}</li>`
+            ))}
+         </ul
+      `)
+   })
+  
+   database.end()
 })
+
 
 app.listen(3000, () => console.log('Server is running on port 3000 🚀'))
